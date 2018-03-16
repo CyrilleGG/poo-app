b9088941eb1b1fdc09df2728149b229539c8e2b0
Cyrille Guiot-Guillin <cyrille.guiot-guillin@etu-webschoolfactory.fr>
2018-03-16 13:04:03 +0100

    test

--- src/templates/index.php
+++ src/templates/index.php
@@ -1,13 +1,28 @@
 <?php include "common/header.php" ?>
 
 <div id="transactions">
-  <h4>Deliveroo</h4>
+  <h1>Bienvenue sur Deliveroo</h1>
   <hr>
-  <div class="container">
+  <div class="container-fluid">
     <div class="row">
-      <?php foreach($data["plats"] as $key => $value) { ?>
-        <div class="col-md-12"><a href="/plat?id=<?php echo $value["id"] ?>"><?php echo $value["nom"] ?></a></div>
-      <?php } ?>
+      <div class="col-3 mx-auto">
+        <div class="row">
+          <img src="./images/joseph-gonzalez-99034-unsplash.jpg" alt="Un croque-madame" class="col-12">
+          <h5 class="col-12 text-center">Un croque-madame ?</h5>
+        </div>
+      </div>
+      <div class="col-3 mx-auto">
+        <div class="row">
+          <img src="./images/thomas-tucker-391058-unsplash.jpg" alt="Une pizza" class="col-12">
+          <h5 class="col-12 text-center">Une pizza ?</h5>
+        </div>
+      </div>
+      <div class="col-3 mx-auto">
+        <div class="row">
+          <img src="./images/eaters-collective-132773-unsplash.jpg" alt="Un plat de pâtes" class="col-12">
+          <h5 class="col-12 text-center">Un plat de pâtes ?</h5>
+        </div>
+      </div>
     </div>
   </div>
 </div>
